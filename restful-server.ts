a3ee786d4dd11e4412fcf3ce2df3ef307b838966
Cheng <allen.cheng@te.com>
2019-07-31 10:15:33 +0800

    commit

--- restful-server.ts
+++ restful-server.ts
@@ -110,7 +110,7 @@ export class RestfulServer {
             this.app.use(bodyParser.json());
             //CORS on ExpressJS
             this.app.use(function (req, res, next) {
-                res.header("Access-Control-Allow-Origin", <string>req.headers['origin']);
+                res.header("Access-Control-Allow-Origin", "*");
                 res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
                 res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
                 res.header('Access-Control-Allow-Credentials', 'true');
